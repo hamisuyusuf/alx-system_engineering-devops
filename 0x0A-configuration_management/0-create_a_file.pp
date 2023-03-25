1e2c095efdd404f414e248154ff5d1f26eda207f
hamisuyusuf <hamisuyusuf180@gmail.com>
2023-03-25 05:50:02 -0700

    All task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Using Puppet, create a file in /tmp
- file {'/tmp/school':
-    ensure => 'file',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+# Using Puppet, create a file
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
     content => 'I love Puppet',
-  }
+}
