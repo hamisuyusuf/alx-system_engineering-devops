2c5a760e86a02d72ac36ba556d9f7a5d2455b516
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-04-13 16:57:09 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-# Fixing find why out why Apache is returning a 500 error
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
